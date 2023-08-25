d1bcfa1b7578fe980ec1176e3d9cc531dff2f316
mnajmytsss <najmytsaqiby22@gmail.com>
2023-08-25 19:03:13 +0700

    updated a login and register features route

---
+++ routes/authRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const { register, login } = require("../controller/authController")
+
+const authRouter = Router()
+
+authRouter.post("/register", register)
+authRouter.post("/login", login)
+
+module.exports = authRouter
